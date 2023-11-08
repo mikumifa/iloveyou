7f927fab38e1ff28d2ef6c9b55e5fb7840cdd2a0
mikumifa <1055069518@qq.com>
2023-11-08 11:04:58 +0800

    延长最后一段, 实现割裂感

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bikaPunchInWeb/",
+  base: "/iloveyou/",
   plugins: [
     vue(),
   ],
